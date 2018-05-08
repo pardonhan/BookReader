acb04023dbc596d7f2e865d6be6485e6229107b6
hanfenglin <han_fenglin@163.com>
2018-05-08 18:17:38 +0800

    权限

--- app/src/main/java/com/justwayward/reader/ui/activity/SplashActivity.java
+++ app/src/main/java/com/justwayward/reader/ui/activity/SplashActivity.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2016 JustWayward Team
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,19 +15,33 @@
  */
 package com.justwayward.reader.ui.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.justwayward.reader.R;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks,
+        EasyPermissions.RationaleCallbacks {
+
+    private static final String[] NECESSARY_PERMISSION = {
+
+             Manifest.permission.READ_EXTERNAL_STORAGE
+            , Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     private Unbinder unbinder;
 
@@ -42,6 +56,24 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         unbinder = ButterKnife.bind(this);
+        onPermissionGranted();
+    }
+
+    @AfterPermissionGranted(100)
+    public void onPermissionGranted() {
+        if (hasNecessaryPermission()) {
+            initActivity();
+        } else {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "邦邦医咖需要申请一些权限,取消可能导致程序不可用",
+                    100,
+                    NECESSARY_PERMISSION);
+        }
+
+    }
+
+    private void initActivity() {
 
         runnable = new Runnable() {
             @Override
@@ -60,6 +92,18 @@ public class SplashActivity extends AppCompatActivity {
         });
     }
 
+
+    private boolean hasNecessaryPermission() {
+        return EasyPermissions.hasPermissions(this, NECESSARY_PERMISSION);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
     private synchronized void goHome() {
         if (!flag) {
             flag = true;
@@ -74,5 +118,47 @@ public class SplashActivity extends AppCompatActivity {
         flag = true;
         tvSkip.removeCallbacks(runnable);
         unbinder.unbind();
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+       // Log.d(TAG, "onPermissionsDenied:" + requestCode + ":" + perms.size());
+
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
+
+    @Override
+    public void onRationaleAccepted(int requestCode) {
+
+    }
+
+    @Override
+    public void onRationaleDenied(int requestCode) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+            String yes = "Yes";
+            String no = "No";
+            if (hasNecessaryPermission()) {
+                initActivity();
+            } else {
+                EasyPermissions.requestPermissions(
+                        this,
+                        "邦邦医咖需要申请一些权限",
+                        100,
+                        NECESSARY_PERMISSION);
+            }
+        }
     }
 }
